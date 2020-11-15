82c87262c2372082e7aeedf9f65e50ab90ca960a
PatrikMuniak <patrik.muniak@gmail.com>
2020-11-15 21:38:39 +0000

    added green moving section, not moving yet

--- receipt-cropper/src/Cropper/Cropper.js
+++ receipt-cropper/src/Cropper/Cropper.js
@@ -2,10 +2,54 @@ import React, { Component, Fragment } from "react";
 import Backdrop from "../components/Backdrop/Backdrop";
 import "./Cropper.css";
 export class Cropper extends Component {
+  canvasRef = React.createRef();
+
+  componentDidUpdate = () => {
+    if (!this.props.src) return;
+    let canvas = this.canvasRef.current;
+
+
+    
+    const ctx = canvas.getContext("2d");
+
+    console.log('canvas',canvas.width, canvas.height);
+    const image = new Image();
+    image.onload = function () {
+      const width = this.width/10;
+      const height = this.height/10;
+        canvas.width = width;
+        canvas.height = height;
+
+      console.log(width, height);
+      
+    //   ctx.fillStyle = "#000000";
+    //   ctx.rect(5, 5, 500, 500);
+    //   ctx.fill()
+      ctx.drawImage(image, 0, 0, this.width, this.height, 0, 0, width, height);
+      ctx.strokeStyle = "#99ff33";
+      ctx.lineWidth = 5;
+      ctx.beginPath();
+      ctx.rect(2, 2, width - 5, height - 5);
+      
+      ctx.stroke();
+    };
+    image.src = this.props.src;
+  };
   getCoordinates = (event) => {
     let x = event.clientX;
     let y = event.clientY;
-    console.log('this is x ', x,'this is y ', y);
+    const domRect = this.canvasRef.current.getBoundingClientRect();
+    console.log(domRect)
+    // bottom: 457.7593765258789
+    // height: 412
+    // left: 193.84375
+    // right: 502.84375
+    // top: 45.759376525878906
+    // width: 309
+    // x: 193.84375
+    // y: 45.759376525878906
+    const coordinate = { x: x - domRect.x, y: y - domRect.y };
+    console.log("coordinates ", coordinate.x, coordinate.y);
   };
 
   render() {
@@ -14,12 +58,11 @@ export class Cropper extends Component {
         {this.props.src ? (
           <Backdrop>
             <div className="crop-box">
-              <img
+              <canvas
                 onClick={this.getCoordinates}
-                src={this.props.src}
-                width="300px"
-                height="420px"
-              />
+                draggable="false"
+                ref={this.canvasRef}
+              ></canvas>
               <button
                 onClick={() => {
                   this.props.srcWiper();
